47567db227e27e00dabdf67e2eb5b2131dc66833
Dominik Strässle <dominik.straessle@gibmit.ch>
2019-11-28 10:46:16 +0100

    Update dbconnector.inc.php

--- dbconnector.inc.php
+++ dbconnector.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'localhost';
-$username = 'IkarusDBUser';
-$password = 'IkarusPasswort'; 
-$database = 'ikarusgamblingsite';
+$host = array_key_exists('DB_HOST', $_ENV) ? $_ENV['DB_HOST'] : 'localhost';
+$username = array_key_exists('DB_USERNAME', $_ENV) ? $_ENV['DB_USERNAME'] : 'IkarusDBUser';
+$password = array_key_exists('DB_PASSWORD', $_ENV) ? $_ENV['DB_PASSWORD'] : 'IkarusPasswort'; 
+$database = array_key_exists('DB_DATABASE', $_ENV) ? $_ENV['DB_DATABASE'] : 'ikarusgamblingsite';
 
 // mit Datenbank verbinden
 $mysqli = new mysqli($host, $username, $password, $database);
